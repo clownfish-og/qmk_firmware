90718c04c69429fe0f1f23d3c4927610bc7acd52
clownfish-og <eriknimmo@gmail.com>
2024-11-08 10:15:59 -0500

    got rid of dev board configs and low power defines

--- keyboards/wind/x98/rules.mk
+++ keyboards/wind/x98/rules.mk
@@ -1,7 +1,2 @@
 # Configure for 128K flash
 MCU_LDSCRIPT = STM32F103xB
-
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
-
-#VIAL_INSECURE = yes    #禁用启动VIAL恶意刷新固件的按键
