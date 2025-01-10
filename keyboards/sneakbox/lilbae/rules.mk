3051c53359da6696d42e405102f60beb66893dc5
clownfish-og <eriknimmo@gmial.com>
2025-01-10 00:33:37 -0500

    rename info.json to keyboard.json, migrate matrix pins to keyboard.json, remove config.h, qmk migrate

--- keyboards/sneakbox/lilbae/rules.mk
+++ keyboards/sneakbox/lilbae/rules.mk
@@ -1,14 +1,10 @@
 # MCU name
-MCU = atmega32u2
 
 # Bootloader selection
-BOOTLOADER = atmel-dfu
 
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -20,4 +16,3 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-ENCODER_ENABLE = yes
