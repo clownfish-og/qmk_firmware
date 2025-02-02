78f2390ba2afd9f991aaaadb2ae78b658d7e8041
clownfish-og <177758267+clownfish-og@users.noreply.github.com>
2025-02-02 15:41:11 -0500

    add keymap test-np

---
+++ keyboards/buppad/buppad25/keymaps/test-np/keymap.c
@@ -0,0 +1,286 @@
+/* Copyright 2024 ClownFish (@clownfish-og)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include <ctype.h>
+#include "keymap.h"
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        TO(1),      CHEESE,     AMNESIA,    GROOVY,     DUCKDANCE,
+        DISCO,      DIDDY,      CATDANCE,   SLAY,       BEARDDANCE,
+        PATBANG,    FREAKOUT,   DRUMS,      FROG,       KEKW,
+        BACKDOOR,   LOVE,       THANKS,     ZEJIBO,     BUG,
+        BITS,       MOOSE,      BUP,        CAPGEN5,    KC_ENT
+    ),
+    [1] = LAYOUT(
+        TO(0),      TO(5),      TO(2),      TO(3),      TO(4),
+        WATER,      DONUT,      DYE,        SMIRK,      EXCUSEME,
+        RUMP,       TINFOIL,    CLOUDS,     SALT,       WINK,
+        BALLOON,    HEART,      ASCEND,     JELLY,      PLUG,
+        RAVE,       CROWN,      TIEDYE,     DRAGON,     SUS
+    ),
+    [2] = LAYOUT(
+        TO(0),      TO(1),      TO(5),      TO(3),      TO(4),
+        DUCKHEAD,   WIZ,        CHEFKISS,   MAPLE,      BIGBRAIN,
+        CALL,       DOIT,       GROGU,      JAWNESSA,   BART,
+        BUPS,       BROC,       BUPWAD,     LUIGIBUP,   ZELDABUP,
+        AYO,        TUNE,       SH,         EE,         ID
+    ),
+    [3] = LAYOUT(
+        TO(0),      TO(1),      TO(2),      TO(5),      TO(4),
+        BASSSFACE,   BOB,        LIGHTER,    UWU,        UP,
+        NOD,        GOOSE,      BOOMER,     WUB,        WOOK,
+        HEADOUT,    RAINBOW,    SALUTE,     SWEATY,     HORN,
+        FLUTE,      FIRE,       CHEERS,     KEYS,       CHILLGUY
+    ),
+    [4] = LAYOUT(
+        TO(0),      TO(1),      TO(2),      TO(3),      TO(5),
+        BDANCE,     BMINGO,     BMONKEY,    BPUG,       BUGH,
+        KGLUTES,    KHI,        KHUG,       KLEI,       KLOVE,
+        KCLAP,      KGATO,      KGGS,       KRDNC,      KSMUG,
+        BOPBOP,     DINODANCE,  UNITY,      KAPPA,      TOMBRAID
+    ),
+    [5] = LAYOUT(
+        TO(0),      TO(1),      TO(2),      TO(3),      TO(4),
+        RGB_MOD,    RGB_VAI,    RGB_HUI,    RGB_SAI,    RGB_SPI,
+        RGB_RMOD,   RGB_VAD,    RGB_HUD,    RGB_SAD,    RGB_SPD,
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+        RGB_M_P,    RGB_M_B,    RGB_M_R,    RGB_M_SW,   RGB_TOG
+    ),
+};
+
+
+
+    const char *suffix[] = {
+    "Amnesia",
+    "Ayo",
+    "BackDoor",
+    "Balloon",
+    "Bart",
+    "BEARDDANCE",
+    "BigBrain",
+    "BITS",
+    "BOOMER",
+    "Bug",
+    "BUP",
+    "BUPS",
+    "BUPWAD",
+    "Call",
+    "CATDANCE",
+    "CHEERS",
+    "Cheese",
+    "Chefkiss",
+    "Crown",
+    "Diddy",
+    "Disco",
+    "DOIT",
+    "Donut",
+    "Drums",
+    "DUCKDANCE",
+    "Duckhead",
+    "Dye",
+    "EE",
+    "Fire",
+    "FLUTE",
+    "Freakout",
+    "FROG",
+    "Grogu",
+    "Groovy",
+    "HEADOUT",
+    "HORN",
+    "ID",
+    "Kekw",
+    "KEYS",
+    "Lighter",
+    "LOVE",
+    "LUIGIBUP",
+    "MOOSE",
+    "PATBANG",
+    "Plug",
+    "Salute",
+    "Sh",
+    "Slay",
+    "Sweaty",
+    "THANKS",
+    "WOOK",
+    "WUB",
+    "Zejibo",
+    "ZELDABUP",
+    "Broc",
+    "Clouds",
+    "Goose",
+    "Heart",
+    "JAWNESSA",
+    "Rave",
+    "Salt",
+    "TINFOIL",
+    "Water",
+    "Wiz",
+    "BASSSFACE",
+    "BOB",
+    "Chillguy",
+    "Dragon",
+    "Jelly",
+    "Maple",
+    "Rump",
+    "Smirk",
+    "UWU",
+    "Wink",
+    "AbDUCKtion",
+    "Ascend",
+    "ASHDUCKEM",
+    "EXCUSEME",
+    "FANCYDUCK",
+    "Gold",
+    "HEADPHONES",
+    "KTULUEHUG",
+    "RAINBOW",
+    "Smug",
+    "Sus",
+    "Nod",
+    "TIEDYE",
+    "TUNE",
+    "Up",
+    "WAVE",
+    "BOP",
+    "BopBop",
+    "DinoDance",
+    "GlitchLit",
+    "GoatEmotey",
+    "GoldPLZ",
+    "Kappa",
+    "Kreygasm",
+    "ModLove",
+    "NotLikeThis",
+    "PowerUpL",
+    "PowerUpR",
+    "SingsMic",
+    "SingsNote",
+    "StinkyCheese",
+    "TombRaid",
+    "TwitchConHYPE",
+    "TwitchLit",
+    "twitchRaid",
+    "TwitchUnity",
+    "VoteNay",
+    "VoteYea",
+    "Dance",
+    "Mingo",
+    "Monkey",
+    "Pug",
+    "Ugh",
+    "Clap",
+    "Gato",
+    "GGs",
+    "GLUTES",
+    "HI",
+    "HUG",
+    "Lei",
+    "Love",
+    "Rdnc",
+    "SMUG"
+};
+
+#define FIRST_EMOTE AMNESIA
+#define LAST_EMOTE KSMUG
+const int start_bup = AMNESIA;
+const int end_bup = WAVE;
+const int start_bex = BDANCE;
+const int end_bex = BUGH;
+const int start_ktlu = KCLAP;
+const int end_ktlu = KSMUG;
+const int first_emote = FIRST_EMOTE;
+const int last_emote = LAST_EMOTE;
+
+const char bup_p[] = "bup";
+const char bex_p[] = "bexfro";
+const char ktlu_p[] = "ktulue";
+
+// This function inverts the capitalization of each character in the given string.
+void invert_caps(char *str) {
+    while (*str) {
+        if (islower(*str)) {
+            *str = toupper(*str);
+        } else if (isupper(*str)) {
+            *str = tolower(*str);
+        }
+        str++;
+    }
+}
+
+bool process_record_bup(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch (keycode) {
+            case CAPGEN5:
+                SEND_STRING(SS_LCTL("acvvvvv"));
+                return false;
+            case CAPGEN8:
+                SEND_STRING(SS_LCTL("acvvvvvvvv"));
+                return false;
+            case CAPGEN10:
+                SEND_STRING(SS_LCTL("acvvvvvvvvvv"));
+                return false;
+            case FIRST_EMOTE ... LAST_EMOTE: {
+                bool caps = host_keyboard_led_state().caps_lock;
+                uint8_t i = keycode - first_emote;
+                char emote_buffer[40];
+                emote_buffer[0] = '\0';
+
+                if (keycode >= start_bup && keycode <= end_bup) {  // Most keycodes use the bup prefix
+                    strlcpy(emote_buffer, bup_p, sizeof(emote_buffer));  // load prefix to buffer
+                    uprintf("Prefix: %s, ", emote_buffer);
+                    strcat(emote_buffer, suffix[i]);                     // append suffix to buffer
+                } else if (keycode >= start_bex && keycode <= end_bex) {   // Handle Bex keycodes
+                    strlcpy(emote_buffer, bex_p, sizeof(emote_buffer));  // load prefix to buffer
+                    uprintf("Prefix: %s, ", emote_buffer);
+                    strcat(emote_buffer, suffix[i]);                     // append suffix to buffer
+                } else if (keycode >= start_ktlu && keycode <= end_ktlu) {  // Handle Ktulue keycodes
+                    strlcpy(emote_buffer, ktlu_p, sizeof(emote_buffer));  // load prefix to buffer
+                    uprintf("Prefix: %s, ", emote_buffer);
+                    strcat(emote_buffer, suffix[i]);                     // append suffix to buffer
+                } else {
+                    strlcpy(emote_buffer, suffix[i], sizeof(emote_buffer));  // Non-prefixed keycodes
+                }
+
+                // Debug output
+                uprintf("Keycode: %u, Suffix: %s, Emote: %s\n", keycode, suffix[i], emote_buffer);
+
+                if (caps) {
+                    invert_caps(emote_buffer);
+                }
+
+                // Append a space and send the emote
+                strlcat(emote_buffer, " ", sizeof(emote_buffer));
+                send_string(emote_buffer);
+                return false;
+            }
+            default:
+                break;
+        }
+    }
+    return true;
+}
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_bup(keycode, record)) {
+        return false;
+    }
+
+    return true;
+}
---
+++ keyboards/buppad/buppad25/keymaps/test-np/keymap.h
@@ -0,0 +1,165 @@
+/* Copyright 2024 ClownFish (@clownfish-og)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "action.h"
+#include "quantum.h"
+
+#ifdef VIA_ENABLE
+#    include "via.h"
+#endif
+
+#include "quantum_keycodes.h"
+
+
+
+enum custom_keycodes {
+    CAPGEN5 = QK_KB_0,
+    CAPGEN8,
+    CAPGEN10,
+    AMNESIA, //begin bup emotes
+    AYO,
+    BACKDOOR,
+    BALLOON,
+    BART,
+    BEARDDANCE,
+    BIGBRAIN,
+    BITS,
+    BOOMER,
+    BUG,
+    BUP,
+    BUPS,
+    BUPWAD,
+    CALL,
+    CATDANCE,
+    CHEERS,
+    CHEESE,
+    CHEFKISS,
+    CROWN,
+    DIDDY,
+    DISCO,
+    DOIT,
+    DONUT,
+    DRUMS,
+    DUCKDANCE,
+    DUCKHEAD,
+    DYE,
+    EE,
+    FIRE,
+    FLUTE,
+    FREAKOUT,
+    FROG,
+    GROGU,
+    GROOVY,
+    HEADOUT,
+    HORN,
+    ID,
+    KEKW,
+    KEYS,
+    LIGHTER,
+    LOVE,
+    LUIGIBUP,
+    MOOSE,
+    PATBANG,
+    PLUG,
+    SALUTE,
+    SH,
+    SLAY,
+    SWEATY,
+    THANKS,
+    WOOK,
+    WUB,
+    ZEJIBO,
+    ZELDABUP,
+    BROC, //begin tier 2000 emotes
+    CLOUDS,
+    GOOSE,
+    HEART,
+    JAWNESSA,
+    RAVE,
+    SALT,
+    TINFOIL,
+    WATER,
+    WIZ,
+    BASSSFACE, //begin tier 3000 emotes
+    BOB,
+    CHILLGUY,
+    DRAGON,
+    JELLY,
+    MAPLE,
+    RUMP,
+    SMIRK,
+    UWU,
+    WINK,
+    ABDUCKTION, //begin bitstier emotes
+    ASCEND,
+    ASHDUCKEM,
+    EXCUSEME,
+    FANCYDUCK,
+    GOLD,
+    HEADPHONES,
+    KTULUEHUG,
+    RAINBOW,
+    SMUG,
+    SUS,
+    NOD, //begin follower emotes
+    TIEDYE,
+    TUNE,
+    UP,
+    WAVE, //end bup emotes
+    BOP, //begin twitch emotes
+    BOPBOP,
+    DINODANCE,
+    GLITCHLIT,
+    GOATEMOTEY,
+    GOLDPLZ,
+    KAPPA,
+    KREYGASM,
+    MODLOVE,
+    NOTLIKETHIS,
+    POWERUPL,
+    POWERUPR,
+    SINGSMIC,
+    SINGSNOTE,
+    STINKYCHEESE,
+    TOMBRAID,
+    TWITCHCONHYPE,
+    TWITCHLIT,
+    TWITCHRAID,
+    UNITY,
+    VOTENAY,
+    VOTEYEA, //end twitch emotes
+    BDANCE, //begin fam emotes
+    BMINGO,
+    BMONKEY,
+    BPUG,
+    BUGH,
+    KCLAP,
+    KGATO,
+    KGGS,
+    KGLUTES,
+    KHI,
+    KHUG,
+    KLEI,
+    KLOVE,
+    KRDNC,
+    KSMUG, //end fam emotes
+};
+
+bool process_record_bup(uint16_t keycode, keyrecord_t *record);
