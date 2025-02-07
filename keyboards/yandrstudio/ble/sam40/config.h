6d2496be7ecf8554006eb5300950cc9421f04f16
clownfish-og <177758267+clownfish-og@users.noreply.github.com>
2025-02-07 10:26:14 -0500

    add yandrstudio/sam40

---
+++ keyboards/yandrstudio/ble/sam40/config.h
@@ -0,0 +1,34 @@
+/* Copyright 2021 JasonRen(biu)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#define DISABLE_MAGIC_BOOTLOADER
+#define DISABLE_EEPROM_CLEAR
+
+// for ble
+// USB Power
+#define USB_PWR_READ
+#define USB_PWR_READ_PIN B10
+// ADC SETTING
+#define SAMPLE_BATTERY
+#define BATTERY_LEVEL_SW_PIN  B1
+// wake pin
+#define BIUSTM32WKPin         A0
+#define BIUNRF52ResetPin      A1
+#define BIUNRF52WKPin         A2
+
+// for ble
+// #   define SLEEP_IN_STOP_MATRIX_LOW
