91c12d1e8c6108824104b7373a36d768ef123590
clownfish-og <177758267+clownfish-og@users.noreply.github.com>
2025-02-07 14:14:59 -0500

    remove default config settings

--- keyboards/buppad/buppad25/keymaps/test/config.h
+++ keyboards/buppad/buppad25/keymaps/test/config.h
@@ -17,8 +17,5 @@
 #pragma once
 
 //disable options to reduce firmware size
-#undef LOCKING_SUPPORT_ENABLE
-#undef LOCKING_RESYNC_ENABLE
 #define NO_ACTION_ONESHOT
-#define NO_MUSIC_MODE
 #define LAYER_STATE_8BIT
