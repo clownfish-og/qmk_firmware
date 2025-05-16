b8b78be39d12d5d78fcebf8d5ad71f0e329398c1
clownfish-og <177758267+clownfish-og@users.noreply.github.com>
2025-05-16 10:40:04 -0400

    more migration to DD, fix broken PWM config

--- keyboards/windstudio/x80/wired/keymaps/default/keymap.c
+++ keyboards/windstudio/x80/wired/keymaps/default/keymap.c
@@ -12,12 +12,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,                                                       KC_RALT, MO(1),   KC_RGUI, KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT
         ),
     [1] = LAYOUT(
-    XXXXXXX,   KC_MSEL, KC_VOLD, KC_VOLU,  KC_MUTE, KC_MSTP, KC_MPRV, KC_MPLY, KC_MNXT,   KC_MAIL, KC_WHOM, KC_CALC, KC_WSCH, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,   RGB_TOG, RGB_MOD, RGB_RMOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI,   RGB_VAD, XXXXXXX, XXXXXXX,          XXXXXXX,
-    XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MKC_MACOS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,   MKC_LG,  XXXXXXX, XXXXXXX,                                                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    _______,   KC_MSEL, KC_VOLD, KC_VOLU,  KC_MUTE, KC_MSTP, KC_MPRV, KC_MPLY, KC_MNXT,   KC_MAIL, KC_WHOM, KC_CALC, KC_WSCH, _______, _______, _______, _______,
+    _______,   _______, _______, _______,  _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,   _______, _______, _______,  _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,   RGB_TOG, RGB_MOD, RGB_RMOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI,   RGB_VAD, _______, _______,          _______,
+    _______,   _______, _______, _______,  _______, _______, _______, _______, MKC_MACOS, _______, _______, _______, _______, _______, _______, _______,
+    _______,   MKC_LG,  _______, _______,                                                          _______, _______, _______, _______, _______, _______, _______
         ),
     [2] = LAYOUT(
     KC_ESC,  KC_BRID,  KC_BRIU, MKC_MCTL, MKC_LPAD, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,  KC_VOLU,  KC_F13,   KC_PSCR, KC_SCRL, KC_PAUS,
@@ -28,29 +28,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, MKC_LOPT, MKC_LCOMM,KC_SPC,                                                         MKC_RCOMM,MO(3),   MKC_ROPT, KC_RCTL,  KC_LEFT, KC_DOWN, KC_RIGHT
         ),
     [3] = LAYOUT(
-    XXXXXXX, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13,  XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, RGB_TOG, RGB_MOD, RGB_RMOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, XXXXXXX, XXXXXXX,          XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MKC_WIN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13,  _______, _______, _______,
+    _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, RGB_TOG, RGB_MOD, RGB_RMOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______,          _______,
+    _______, _______, _______, _______,  _______, _______, _______, _______, MKC_WIN, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______,                                                        _______, _______, _______, _______, _______, _______, _______
         )
 };
-
-#ifdef RGB_MATRIX_ENABLE
-
-led_config_t g_led_config = {{    //通过使用按键的电子矩阵行和列告诉系统此 LED 代表什么按键
-    //      C0,     C1,     C2,      C3,     C4      C5      C6      C7       C8        C9     C10       C11     C12     C13     C14,    C15,     C16,      C17,       C18
-    {        0,      1,      2,                 },  //19
-  //4
-
-}, {   //C0           C1          C2         C3         C4          C5         C6          C7          C8          C9             C10            C11         C12            C13            c14,            C15          C16            C17          C18
-       //LED Index to Physical Position   //在键盘上的物理位置
-    { 0,  0 }, { 14,  0 }, { 28,  0 },
-
-  }, {
-  //0  1  2  3  4  5  6  7  8  9  10 11 12 13 14  15 16 17 18
-    1, 4, 4,}
-};
-
-#endif
