bdd6b5e298c36886ac5046b3a8660676713f73d3
clownfish-og <177758267+clownfish-og@users.noreply.github.com>
2025-07-15 11:13:12 -0400

    tested working, no rgblight

--- keyboards/yandrstudio/transition_lite_1k/config.h
+++ keyboards/yandrstudio/transition_lite_1k/config.h
@@ -16,19 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
 
-#define WS2812_RGBLIGHT_DI_PIN              A2
-#define WS2812_RGBLIGHT_PWM_DRIVER           PWMD2               /** TIMx        对应x*/
-#define WS2812_RGBLIGHT_PWM_CHANNEL          3                   /** TIMx_CHy    对应y */
-#define WS2812_RGBLIGHT_PWM_PAL_MODE         2
-#define WS2812_RGBLIGHT_DMA_STREAM           STM32_DMA1_STREAM2  /** TIMx_UP     对应x的UP，DMA数据表*/
-#define WS2812_RGBLIGHT_DMA_CHANNEL          2                   /** STM32_DMAx_STREAMy 对应y*/
-
-#ifdef RGBLIGHT_ENABLE
-#   define RGBLIGHT_LAYERS
-#   define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
-#   define RGBLIGHT_LAYERS_RETAIN_VAL
-#endif
-
 #ifdef RGB_MATRIX_ENABLE
 // /** RGB MATRIX CONFIGS ----- B13 TIM1_CH1N */
 #   define WS2812_PWM_COMPLEMENTARY_OUTPUT                 /** TIMx_CHyN   需要带N结尾*/
@@ -38,8 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #   define WS2812_DMA_STREAM           STM32_DMA1_STREAM5  /** TIMx_UP     对应x的UP，DMA数据表*/
 #   define WS2812_DMA_CHANNEL          5                   /** STM32_DMAx_STREAMy 对应y*/
 
-#   define RGB_MATRIX_KEYPRESSES
-#   define RGB_MATRIX_DISABLE_KEYCODES
 
 #   define RGB_INDEX_CAPS                  62  /** Caps 指示灯索引 **/
 #   define RGB_INDEX_SCROLL                14  /** Scorll 指示灯索引 **/
