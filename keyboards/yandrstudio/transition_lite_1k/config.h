71db56fa147e695db74dbb0d43de4649fd7da5be
clownfish-og <177758267+clownfish-og@users.noreply.github.com>
2025-07-21 13:28:44 -0400

    working

--- keyboards/yandrstudio/transition_lite_1k/config.h
+++ keyboards/yandrstudio/transition_lite_1k/config.h
@@ -17,15 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-// /* RGBLIGHT CONFIGS ----- A2 TIM2_CH3 */
-#define WS2812_RGBLIGHT_DI_PIN              A2
-// #define WS2812_RGBLIGHT_PWM_COMPLEMENTARY_OUTPUT                /** TIMx_CHyN   需要带N结尾*/
-#define WS2812_RGBLIGHT_PWM_DRIVER           PWMD2               /** TIMx        对应x*/
-#define WS2812_RGBLIGHT_PWM_CHANNEL          3                   /** TIMx_CHy    对应y */
-#define WS2812_RGBLIGHT_PWM_PAL_MODE         2
-#define WS2812_RGBLIGHT_DMA_STREAM           STM32_DMA1_STREAM2  /** TIMx_UP     对应x的UP，DMA数据表*/
-#define WS2812_RGBLIGHT_DMA_CHANNEL          2                   /** STM32_DMAx_STREAMy 对应y*/
-
 #ifdef RGBLIGHT_ENABLE
 #   define RGBLIGHT_LAYERS
 #   define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
@@ -34,19 +25,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #ifdef RGB_MATRIX_ENABLE
-// /** RGB MATRIX CONFIGS ----- B13 TIM1_CH1N */
-#define WS2812_PWM_COMPLEMENTARY_OUTPUT                 /** TIMx_CHyN   需要带N结尾*/
-#define WS2812_PWM_DRIVER           PWMD1               /** TIMx        对应x*/
-#define WS2812_PWM_CHANNEL          1                   /** TIMx_CHy    对应y */
-#define WS2812_PWM_PAL_MODE         2
-#define WS2812_DMA_STREAM           STM32_DMA1_STREAM5  /** TIMx_UP     对应x的UP，DMA数据表*/
-#define WS2812_DMA_CHANNEL          5                   /** STM32_DMAx_STREAMy 对应y*/
-
 #   define RGB_MATRIX_KEYPRESSES
 #   define RGB_MATRIX_DISABLE_SHARED_KEYCODES
 
-#   define RBG_INDEX_CAPS                  62  /** Caps 指示灯索引 **/
-#   define RBG_INDEX_SCROLL                14  /** Scorll 指示灯索引 **/
-#   define RBG_INDEX_FN                    80  /** FN   指示灯索引 **/
+#   define RGB_INDEX_CAPS                  62  /** Caps 指示灯索引 **/
+#   define RGB_INDEX_SCROLL                14  /** Scorll 指示灯索引 **/
+#   define RGB_INDEX_FN                    80  /** FN   指示灯索引 **/
 #endif
 
--- keyboards/yandrstudio/transition_lite_1k/keymaps/default/keymap.c
+++ keyboards/yandrstudio/transition_lite_1k/keymaps/default/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT,            KC_UP,
-        KC_LCTL, KC_LGUI, KC_LALT,                            LT(_FN, KC_SPC),                                      KC_RALT, MO(_FN), KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            LT(_FN, KC_SPC),                    KC_RALT, KC_RGUI, MO(_FN), KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FN] = LAYOUT_tkl_ansi(
         // 1     2        3        4        5        6        7        8        9        10       11       12        13      14         15       16       17
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RM_TOGG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   UG_HUED, UG_SATD, UG_SPDD,
         RM_NEXT, RM_VALU, RM_HUEU, RM_SATU, RM_SPDU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
         RM_PREV, RM_VALD, RM_HUED, RM_SATD, RM_SPDD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,            UG_VALU,
-        KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                                     MO(_FUN),KC_TRNS, KC_TRNS,   UG_PREV,UG_VALD, UG_NEXT
+        KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, MO(_FUN),KC_TRNS, KC_TRNS,   UG_PREV,UG_VALD, UG_NEXT
     ),
     [_FUN] = LAYOUT_tkl_ansi(
         // 1     2        3        4        5        6        7        8        9        10       11       12        13      14         15       16       17
@@ -63,6 +63,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,            KC_TRNS,
-        QK_BOOT, KC_TRNS, KC_TRNS,                            KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
+        QK_BOOT, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
