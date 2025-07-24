ca74cd2b3e14c7244968c049f7bc3c027edba332
clownfish-og <177758267+clownfish-og@users.noreply.github.com>
2025-07-24 13:52:41 -0400

    dual PWM

--- keyboards/yandrstudio/transition_lite_1k/config.h
+++ keyboards/yandrstudio/transition_lite_1k/config.h
@@ -22,31 +22,31 @@
 #define WS2812_DMA_CHANNEL          2                   // STM32_DMAx_STREAMy
 */
 
-// A2 TIM2_CH3
-#define WS2812_RGBLIGHT_DI_PIN              A2
-// #define WS2812_RGBLIGHT_PWM_COMPLEMENTARY_OUTPUT                // TIMx_CHyN
-#define WS2812_RGBLIGHT_PWM_DRIVER           PWMD2               // TIMx
-#define WS2812_RGBLIGHT_PWM_CHANNEL          3                   // TIMx_CHy
-#define WS2812_RGBLIGHT_PWM_PAL_MODE         2
-#define WS2812_RGBLIGHT_DMA_STREAM           STM32_DMA1_STREAM2  // TIMx_UP
-#define WS2812_RGBLIGHT_DMA_CHANNEL          2                   // STM32_DMAx_STREAMy
+// PWM and DMA configuration for TIM2_CH3 (A2)
+#define RGBLIGHT_DI_PIN             A2
+#define RGBLIGHT_PWM_PAL_MODE       2
+#define RGBLIGHT_PWM_DRIVER         PWMD2
+#define RGBLIGHT_PWM_CHANNEL        3
+#define RGBLIGHT_DMA_STREAM         STM32_DMA1_STREAM2
+#define RGBLIGHT_DMA_CHANNEL        2
 
 #ifdef RGBLIGHT_ENABLE
+#   define RGBLIGHT_LED_COUNT 26
 #   define RGBLIGHT_LAYERS
 #   define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
 #   define RGBLIGHT_LAYERS_RETAIN_VAL
 #   define RGBLIGHT_DEFAULT_MODE       RGBLIGHT_MODE_RAINBOW_SWIRL
 #endif
 
-#ifdef RGB_MATRIX_ENABLE
 // /** RGB MATRIX CONFIGS ----- B13 TIM1_CH1N */
 #define WS2812_PWM_COMPLEMENTARY_OUTPUT                 // TIMx_CHyN
 #define WS2812_PWM_DRIVER           PWMD1               // TIMx
 #define WS2812_PWM_CHANNEL          1                   // TIMx_CHy
 #define WS2812_PWM_PAL_MODE         2
-#define WS2812_DMA_STREAM           STM32_DMA1_STREAM5  // TIMx_UP
-#define WS2812_DMA_CHANNEL          5                   // STM32_DMAx_STREAMy
+#define WS2812_PWM_DMA_STREAM       STM32_DMA1_STREAM5  // TIMx_UP
+#define WS2812_PWM_DMA_CHANNEL      5                   // STM32_DMAx_STREAMy
 
+#ifdef RGB_MATRIX_ENABLE
 #   define RGB_MATRIX_KEYPRESSES
 #   define RGB_MATRIX_DISABLE_SHARED_KEYCODES
 
